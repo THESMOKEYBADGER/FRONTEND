535d795c6b6d1784dd134e8443e1928218383778
THESMOKEYBADGER <gabrielrobbertze@gmail.com>
2023-10-31 12:43:57 +0200

    Frontend upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FruitCreateComponent } from './fruit/fruit-create/fruit-create.component';
 import { FruitDisplayComponent } from './fruit/fruit-display/fruit-display.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FruitCreateComponent,
-    FruitDisplayComponent
+    FruitDisplayComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/auth-service.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  public token!: string;
+
+  constructor(private http: HttpClient) { }
+
+  signup (userusername:string, userpassword: string)
+  {
+    this.http.post('https://localhost:3000/api/users/signup', {username:userusername, passsword:userpassword})
+    .subscribe(response =>{
+    });
+  }
+
+  login (userusername:string, userpassword: string)
+  {
+    this.http.post<{token: string}>('https://localhost:3000/api/users/login', {username:userusername, password:userpassword})
+    .subscribe(response =>{
+      const token = response.token;;
+      this.token = token;
+    });
+  }
+
+  getToken(){
+    return this.token;
+  }
+
+}
+
